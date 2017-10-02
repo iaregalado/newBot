72d1f85ee297eca2f23f298b6dc7db62d0d30e93
iaregalado <iaregalado@gmail.com>
2017-10-01 19:03:09 -0700

    simpleCalc and cuss finder

--- scripts/newBot.js
+++ scripts/newBot.js
@@ -1,5 +1,5 @@
 // Description:
-// <description of this script's functionality>
+// Simple calculator for add, substract, multiply and divide.  Also, catches people that cuss!
 //
 // Dependencies:
 //   None
@@ -8,9 +8,9 @@
 //   None
 //
 // Commands:
-//  hubot <trigger> - <what the respond trigger does>
-//  <trigger> - <what the hear trigger does>
-//
+//  hubot string starting with calculation to be performed, followed by two digits - hubot will perform calculation
+//  Sample:
+//  @newBot add 1 plus 2
 // Notes:
 //
 //
@@ -49,23 +49,10 @@ module.exports = function(robot) {
 
   console.log(total);
 
-  //return msg.reply("Adding " + firstNumber + " and " + secondNumber + " equals " + total);
-
   });
 
-  //  YOUR CODE HERE
-  //  Example
-//    robot.respond(/I like (.*)/i, function(msg) {
-//      var name;
-//      name = msg.Match[0];
-//      if(name == 'javascript'){
-//        return msg.send("I love coding!");
-//      } else {
-//        return msg.send("I love eating!");
-//      }  
-//    });
-
-robot.hear(/(.*)/i, function(msg) {
+//find people cussing!
+  robot.hear(/(.*)/i, function(msg) {
   var swearWord = msg.match[1];
   console.log(swearWord);
   var swearJar = ["buck", "bucking"];
@@ -75,33 +62,6 @@ robot.hear(/(.*)/i, function(msg) {
       return msg.reply("Hi there, we don't like to cuss here!");
     }
   }
-});
+  });
 
 }
-
-/************************************
-EXAMPLES OF THE KEY HUBOT FUNCTIONS
-************************************/
-
-
-/* IMPORTANT! 
-You can have only one instance of module.exports in each JavaScript file. 
-If you want to supplement your existing code above with any the code below, 
-you need to move the contents of module.exports below into the module.exports code above. 
-*/
-
-/*
-var squirrels;
-squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
-module.exports = function(robot) {
-  // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" 
-  return robot.respond(/hi|hello/i, function(msg) {
-    return msg.send("Howdy!");
-  });
-  // Random Example
-  //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
-  return robot.hear(/ship it/i, function(msg) {
-    return msg.send(msg.random(squirrels));
-  });
-};
-*/
